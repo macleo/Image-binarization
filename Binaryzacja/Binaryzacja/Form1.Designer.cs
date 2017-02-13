33e19fe7b940d3cc320171ee818f5d2a87edd460
macleo <52macleo@gmail.com>
2017-02-13 15:20:12 +0800

    change UI pictureBox location

--- Binaryzacja/Binaryzacja/Form1.Designer.cs
+++ Binaryzacja/Binaryzacja/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.buttonMain = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.thresholdLabel = new System.Windows.Forms.Label();
@@ -62,18 +62,18 @@
             // 
             this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(26, 78);
             this.chart1.Margin = new System.Windows.Forms.Padding(6);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(1894, 279);
             this.chart1.TabIndex = 1;
             this.chart1.Text = "chart1";
@@ -113,7 +113,7 @@
             // 
             // pixtureBoxOriginalImage
             // 
-            this.pixtureBoxOriginalImage.Location = new System.Drawing.Point(140, 150);
+            this.pixtureBoxOriginalImage.Location = new System.Drawing.Point(750, 14);
             this.pixtureBoxOriginalImage.Margin = new System.Windows.Forms.Padding(6);
             this.pixtureBoxOriginalImage.Name = "pixtureBoxOriginalImage";
             this.pixtureBoxOriginalImage.Size = new System.Drawing.Size(162, 124);
@@ -123,7 +123,7 @@
             // 
             // pictureBoxOtsu
             // 
-            this.pictureBoxOtsu.Location = new System.Drawing.Point(750, 150);
+            this.pictureBoxOtsu.Location = new System.Drawing.Point(750, 162);
             this.pictureBoxOtsu.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBoxOtsu.Name = "pictureBoxOtsu";
             this.pictureBoxOtsu.Size = new System.Drawing.Size(151, 124);
@@ -133,7 +133,7 @@
             // 
             // pictureBoxBernsen
             // 
-            this.pictureBoxBernsen.Location = new System.Drawing.Point(1366, 127);
+            this.pictureBoxBernsen.Location = new System.Drawing.Point(736, 318);
             this.pictureBoxBernsen.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBoxBernsen.Name = "pictureBoxBernsen";
             this.pictureBoxBernsen.Size = new System.Drawing.Size(176, 136);
